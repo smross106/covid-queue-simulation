5c0a13e7a1ccd885a36ae100f5c7096db6f7aa1e
= <smross106@gmail.com>
2020-04-02 14:41:21 +0100

    Added a second strategy - avoid preferred shop if queue exists

--- app0.py
+++ app0.py
@@ -1,3 +1,10 @@
 def blind(shop):
     queue = len(shop.queue)
     return(True)
+
+def block(shop):
+    queue = len(shop.queue)
+    if queue>shop.throughput:
+        return(False)
+    else:
+        return(True)
