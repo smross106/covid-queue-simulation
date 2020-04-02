bcb7c898a5d766bff2c9e590c6c2b03dc264eda4
= <smross106@gmail.com>
2020-04-02 16:26:35 +0100

    Added helper.py for functions common between display and output

---
+++ helper.py
@@ -0,0 +1,29 @@
+import csv
+from actor import Shop, Neighbourhood, Actor
+from random import randint
+
+def setup(gridWidth,nShops):
+    gridSize = 8
+    gridOffset = 50
+    #gridWidth = 50
+    #nShops = int(gridWidth**2 / 100)
+    #TOTALLY ARBITRARY DECISION of 100 people shopping per day per shop
+    shops = []
+    for i in range(0,nShops):
+        shops.append(Shop([(gridSize*randint(0,gridWidth))+gridOffset,(gridSize*randint(0,gridWidth))+gridOffset],10))
+    hood = Neighbourhood(shops)
+    shopLocations = [i.location for i in hood.shops]
+    actors = []
+    for i in range(0,gridWidth):
+        for j in range(0,gridWidth):
+            if not ([(gridSize*i)+gridOffset,(gridSize*j)+gridOffset] in shopLocations):
+                new = Actor([(gridSize*i)+gridOffset,(gridSize*j)+gridOffset],randint(0,1))
+                actors.append(new)
+    
+    return(actors,hood)
+
+def writeDataToCSV(data,filename):
+    with open(filename+".csv","w",newline="") as csvfile:
+        datawriter = csv.writer(csvfile,delimiter=",",
+        quotechar="|",quoting=csv.QUOTE_MINIMAL)
+        datawriter.writerows(data)
