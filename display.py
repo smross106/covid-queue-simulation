8a2e89d4eb3db22defac41de111973b6674b54ed
= <smross106@gmail.com>
2020-04-02 16:27:35 +0100

    Removed duplicated functions

--- display.py
+++ display.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 from actor import Actor, Neighbourhood, Shop
 from app0 import *
 from random import randint
+import helper
 
 pygame.init()
 screen = pygame.display.set_mode([1400,800])
@@ -13,24 +14,7 @@ hour = 0
 
 running = True
 
-def setup():
-    gridSize = 8
-    gridOffset = 50
-    gridWidth = 50
-    nShops = 4
-    shops = []
-    for i in range(0,nShops):
-        shops.append(Shop([(gridSize*randint(0,gridWidth))+gridOffset,(gridSize*randint(0,gridWidth))+gridOffset],5))
-    hood = Neighbourhood(shops)
-    shopLocations = [i.location for i in hood.shops]
-    actors = []
-    for i in range(0,gridWidth):
-        for j in range(0,gridWidth):
-            if not ([(gridSize*i)+gridOffset,(gridSize*j)+gridOffset] in shopLocations):
-                new = Actor([(gridSize*i)+gridOffset,(gridSize*j)+gridOffset],randint(0,1))
-                actors.append(new)
-    
-    return(actors,hood)
+
 
 def graph(screen,x,y,xScale,yScale,data,limit):
 
@@ -52,7 +36,7 @@ def graph(screen,x,y,xScale,yScale,data,limit):
         pygame.draw.line(screen,col,(x+int(i*xScale),y-int(point*yScale)),(x+int((i+1)*xScale),y-int(nextPoint*yScale)),1)
 
 
-actors,hood = setup()
+actors,hood = helper.setup(64,10)
 
 while running:
     for event in pygame.event.get():
@@ -63,7 +47,7 @@ while running:
 
     #Update all actor states every 10 ticks
     if tick%10==0:
-        print("\t",hour)
+        print("\t",str(int(hour))+":"+str(int(hour%1 * 60)))
         for i in actors:
             i.run(hour,hood,[blind,block])
         
@@ -75,28 +59,18 @@ while running:
         i.draw(screen,tick%10)
 
     #Draw graph
-    for i in range(0,2):
+    for i in range(0,1):
         xScale = 5/3.
         yScale = 5
         graph(screen,800,200+(25*yScale + 100)*i,xScale,yScale,hood.shops[i].historicQueue,hood.shops[i].throughput)
+    """
     for i in range(0,2):
         xScale = 5/3.
         yScale = 5
         graph(screen,1100,200+(25*yScale + 100)*i,xScale,yScale,hood.shops[i+2].historicQueue,hood.shops[i+2].throughput)
-    """
-    xScale = 5/3.
-    yScale = 5
-    pygame.draw.line(screen,(255,255,255),(100,400),(100,300))
-    pygame.draw.line(screen,(255,255,255),(100,400),(340,400))
-    pygame.draw.line(screen,(255,0,0),(100,400-(yScale*hood.shops[0].throughput)),(340,400-(yScale*hood.shops[0].throughput)))
-    for i in range(0,len(hood.shops[0].historicQueue)-1):
-        point = hood.shops[0].historicQueue[i]
-        nextPoint = hood.shops[0].historicQueue[i+1]
-        col = (0,255,0)
-        pygame.draw.line(screen,col,(100+int(i*xScale),400-int(point*yScale)),(100+int((i+1)*xScale),400-int(nextPoint*yScale)),1)
-    """
+"""
 
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(90)
     tick += 1
     hour = (tick/60)%24
